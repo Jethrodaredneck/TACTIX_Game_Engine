4ff81601e244ec290001c5d63832dabb7a9eaef7
Jethrodaredneck <elijahshadowen1981@gmail.com>
2026-08-08 11:01:12 -0500

    Disable unfinished console panel for editor polish build

--- Editor/Tools/SceneEditorTool.cs
+++ Editor/Tools/SceneEditorTool.cs
@@ -5,7 +5,6 @@ using TACTIX.Editor.UI.Docking;
 using TACTIX.Editor.UI.Panels;
 using TACTIX.Editor.UI.Viewport;
 using TACTIX.Editor.UI.Content;
-using TACTIX.Editor.UI.Console;
 using TACTIX.Editor.AI;
 using TACTIX.Engine.AI;
 using TACTIX.Engine.Runtime.Scene;
@@ -55,7 +54,7 @@ public sealed class SceneEditorTool : EditorTool
             () => new ContentBrowserPanelView(new CGRect(0,0,720,240), _assets.ProjectRoot)));
 
         dockManager.RegisterPanel(new DockPanel("Scene.Console", "Console",
-            () => new ConsolePanelView(new CGRect(0,0,720,240))));
+            () => Placeholder("Console coming later")));
 
         dockManager.RegisterPanel(new DockPanel("Scene.AIBridge", "AI Bridge",
             () => new AIBridgePanelView(new CGRect(0, 0, 520, 260), _aiBridge)));
